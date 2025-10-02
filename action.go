9fe64653ff94dbe26198b7c4e9f5216bd45375f5
yra <ymz.ncnk@outlook.com>
2025-10-02 03:10:15 +0100

    Move Action type definition to dedicated file

---
+++ action.go
@@ -0,0 +1,15 @@
+package idempotency
+
+import "context"
+
+// Action defines the function signature for the core idempotent operation
+// to be executed by the Wrapper.
+//
+// An Action takes the current execution context, a dependency bundle (T)
+// for data access, and the call input (I). It is expected to return
+// the successful operation output (S) or a standard Go error.
+//
+// The Action's failure errors must be mappable by the Wrapper to a failure
+// output so they can be persisted and replayed to the caller on subsequent
+// attempts.
+type Action[T, I, S any] func(ctx context.Context, repos T, input I) (S, error)
--- wrapper.go
+++ wrapper.go
@@ -5,18 +5,6 @@ import (
 	"fmt"
 )
 
-// Action defines the function signature for the core idempotent operation
-// to be executed by the Wrapper.
-//
-// An Action takes the current execution context, a dependency bundle (T)
-// for data access, and the call input (I). It is expected to return
-// the successful operation output (S) or a standard Go error.
-//
-// The Action's failure errors must be mappable by the Wrapper to a failure
-// output so they can be persisted and replayed to the caller on subsequent
-// attempts.
-type Action[T, I, S any] func(ctx context.Context, repos T, input I) (S, error)
-
 // ErrorToFail defines the function that converts a Go 'error' into the
 // storable failure output ('F').
 type ErrorToFail[F any] func(err error) (F, bool)
